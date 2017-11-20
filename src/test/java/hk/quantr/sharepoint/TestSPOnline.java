cf8170f7b425aead530be98daf02191169029023
Peter <peter@quantr.hk>
2017-11-20 12:53:42 +0800

    dev

--- src/test/java/hk/quantr/sharepoint/TestSPOnline.java
+++ src/test/java/hk/quantr/sharepoint/TestSPOnline.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
