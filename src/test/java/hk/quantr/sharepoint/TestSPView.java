7af8ce79a9a16f45af40a39e1680a0b4c1afddef
Peter <peter@quantr.hk>
2017-12-19 17:39:26 +0800

    dev

--- src/test/java/hk/quantr/sharepoint/TestSPView.java
+++ src/test/java/hk/quantr/sharepoint/TestSPView.java
@@ -8,9 +8,9 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.List;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
@@ -24,6 +24,7 @@ public class TestSPView {
 	@Test
 	public void test1() {
 		try {
+			Logger.getLogger(TestSPOnline.class).info("test1");
 			List<String> lines = IOUtils.readLines(new FileReader(System.getProperty("user.home") + File.separator + "password.txt"));
 			String password = lines.get(0);
 			String domain = "quantr";
@@ -35,40 +36,52 @@ public class TestSPView {
 				String formDigestValue = json.getJSONObject("d").getJSONObject("GetContextWebInformation").getString("FormDigestValue");
 				System.out.println("FormDigestValue=" + formDigestValue);
 
-				// print all view names
-				jsonString = SPOnline.get(token, domain, "/test/_api/web/lists/GetByTitle('doclib1')/views");
-				if (jsonString != null) {
-					json = new JSONObject(jsonString);
-					JSONArray arr = json.getJSONObject("d").getJSONArray("results");
-					for (int x = 0; x < arr.length(); x++) {
-						System.out.println(arr.getJSONObject(x).getString("Title"));
-					}
-				}
-
+//				// print all view names
+//				jsonString = SPOnline.get(token, domain, "/test/_api/web/lists/GetByTitle('doclib1')/views");
+//				if (jsonString != null) {
+//					json = new JSONObject(jsonString);
+//					JSONArray arr = json.getJSONObject("d").getJSONArray("results");
+//					for (int x = 0; x < arr.length(); x++) {
+//						System.out.println(arr.getJSONObject(x).getString("Title"));
+//					}
+//				}
+//
 				// get specific view
 				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/views/getbytitle('" + SPOnline.escapeSharePointUrl("peter view呀") + "')");
 				if (jsonString != null) {
 					System.out.println(CommonLib.prettyFormatJson(jsonString));
 				}
+//
+//				// get all fields of a specific view
+//				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/views/getbytitle('" + SPOnline.escapeSharePointUrl("peter view呀") + "')/ViewFields");
+//				if (jsonString != null) {
+//					System.out.println(CommonLib.prettyFormatJson(jsonString));
+//					json = new JSONObject(jsonString);
+//					JSONArray arr = json.getJSONObject("d").getJSONObject("Items").getJSONArray("results");
+//					for (int x = 0; x < arr.length(); x++) {
+//						System.out.println(arr.getString(x));
+//					}
+//				}
 
-				// get all fields of a specific view
-				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/views/getbytitle('" + SPOnline.escapeSharePointUrl("peter view呀") + "')/ViewFields");
+				// get items of a specific view
+				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/views/getbytitle('" + SPOnline.escapeSharePointUrl("peter view呀") + "')/items");
 				if (jsonString != null) {
 					System.out.println(CommonLib.prettyFormatJson(jsonString));
-					json = new JSONObject(jsonString);
-					JSONArray arr = json.getJSONObject("d").getJSONObject("Items").getJSONArray("results");
-					for (int x = 0; x < arr.length(); x++) {
-						System.out.println(arr.getString(x));
-					}
+//					json = new JSONObject(jsonString);
+//					JSONArray arr = json.getJSONObject("d").getJSONObject("Items").getJSONArray("results");
+//					for (int x = 0; x < arr.length(); x++) {
+//						System.out.println(arr.getString(x));
+//					}
+				} else {
+					System.out.println("fuck");
 				}
-
 			} else {
 				System.err.println("Login failed");
 			}
 		} catch (FileNotFoundException ex) {
-			Logger.getLogger(TestSPOnline.class.getName()).log(Level.SEVERE, null, ex);
+			Logger.getLogger(TestSPOnline.class.getName()).error(ex);
 		} catch (IOException ex) {
-			Logger.getLogger(TestSPOnline.class.getName()).log(Level.SEVERE, null, ex);
+			Logger.getLogger(TestSPOnline.class.getName()).error(ex);
 		}
 	}
 }
