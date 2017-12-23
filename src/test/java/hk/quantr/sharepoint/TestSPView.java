f1beb5e5ad5383f68bcdf7d4122e76ddc6708e64
Peter <peter@quantr.hk>
2017-12-23 16:37:09 +0800

    dev

--- src/test/java/hk/quantr/sharepoint/TestSPView.java
+++ src/test/java/hk/quantr/sharepoint/TestSPView.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.tuple.Pair;
@@ -20,62 +21,61 @@ import org.junit.Test;
 public class TestSPView {
 
 	@Test
-	public void test1() {
+	public void getAllItemsWithFieldsThatSpecificByView() {
 		try {
-			Logger.getLogger(TestSPOnline.class).info("test1");
+			Logger.getLogger(TestSPOnline.class).info("getAllItemsWithFieldsThatSpecificByView");
+			String view = "peter view呀";
+
 			List<String> lines = IOUtils.readLines(new FileReader(System.getProperty("user.home") + File.separator + "password.txt"));
 			String password = lines.get(0);
 			String domain = "quantr";
 			Pair<String, String> token = SPOnline.login("wordpress@quantr.hk", password, domain);
 			if (token != null) {
-				String jsonString = SPOnline.post(token, domain, "/_api/contextinfo", null, null);
-				System.out.println(CommonLib.prettyFormatJson(jsonString));
-				JSONObject json = new JSONObject(jsonString);
-				String formDigestValue = json.getJSONObject("d").getJSONObject("GetContextWebInformation").getString("FormDigestValue");
-				System.out.println("FormDigestValue=" + formDigestValue);
+				JSONObject json;
+				String jsonString;
 
-//				// print all view names
-//				jsonString = SPOnline.get(token, domain, "/test/_api/web/lists/GetByTitle('doclib1')/views");
-//				if (jsonString != null) {
-//					json = new JSONObject(jsonString);
-//					JSONArray arr = json.getJSONObject("d").getJSONArray("results");
-//					for (int x = 0; x < arr.length(); x++) {
-//						System.out.println(arr.getJSONObject(x).getString("Title"));
-//					}
-//				}
-//
-				// get specific view
-				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/views/getbytitle('" + SPOnline.escapeSharePointUrl("peter view呀") + "')");
-				if (jsonString != null) {
-					System.out.println(CommonLib.prettyFormatJson(jsonString));
-				}
-
-				// get all fields of a specific view
-				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/views/getbytitle('" + SPOnline.escapeSharePointUrl("peter view呀") + "')/ViewFields");
-				String items = "";
-				if (jsonString != null) {
-					System.out.println(CommonLib.prettyFormatJson(jsonString));
-					json = new JSONObject(jsonString);
-					JSONArray arr = json.getJSONObject("d").getJSONObject("Items").getJSONArray("results");
-					for (int x = 0; x < arr.length(); x++) {
-						System.out.println(arr.getString(x));
-						items += arr.getString(x) + ",";
-					}
-				}
+				HashMap<String, Integer> fieldtypes = new HashMap<String, Integer>();
 
 				// get all fields
 				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/fields");
 				if (jsonString != null) {
 					System.out.println(CommonLib.prettyFormatJson(jsonString));
-//					json = new JSONObject(jsonString);
-//					JSONArray arr = json.getJSONObject("d").getJSONObject("Items").getJSONArray("results");
-//					for (int x = 0; x < arr.length(); x++) {
-//						System.out.println(arr.getString(x));
-//					}
-				} 
-				
+					json = new JSONObject(jsonString);
+					JSONArray arr = json.getJSONObject("d").getJSONArray("results");
+					for (int x = 0; x < arr.length(); x++) {
+						System.out.println(arr.getJSONObject(x).getString("InternalName") + "\t\t\t" + arr.getJSONObject(x).getInt("FieldTypeKind"));
+						fieldtypes.put(arr.getJSONObject(x).getString("InternalName"), arr.getJSONObject(x).getInt("FieldTypeKind"));
+					}
+				}
+
+				String query = "";
+				String expand = "&expand=";
+
+				// get all fields of a specific view
+				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/views/getbytitle('" + SPOnline.escapeSharePointUrl(view) + "')/ViewFields");
+				if (jsonString != null) {
+					System.out.println(CommonLib.prettyFormatJson(jsonString));
+					json = new JSONObject(jsonString);
+					JSONArray arr = json.getJSONObject("d").getJSONObject("Items").getJSONArray("results");
+					for (int x = 0; x < arr.length(); x++) {
+						String fieldName = arr.getString(x);
+						System.out.println(fieldName);
+						if (fieldtypes.get(fieldName) == 12) {
+							query += arr.getString(x) + "/Title,";
+							expand += arr.getString(x) + ",";
+						} else {
+							query += arr.getString(x) + ",";
+						}
+					}
+				}
+
+				if (query.endsWith(",")) {
+					query = query.substring(0, query.length() - 1);
+				}
 				// get items of a specific view
-				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/items?$select=LinkFilename,DocIcon,Modified,Editor/Title&$expand=Editor");
+				//jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/items?$select=LinkFilename,DocIcon,Modified,Editor/Title&$expand=Editor");
+				System.out.println("test/_api/web/lists/GetByTitle('doclib1')/items?$select=" + query + expand);
+				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/items?$select=" + query + expand);
 				if (jsonString != null) {
 					System.out.println(CommonLib.prettyFormatJson(jsonString));
 				}
