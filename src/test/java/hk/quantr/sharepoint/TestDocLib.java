3deb8eac8fec05f5d1ef6912ab45ff379944e915
Peter Cheung <mcheung63@hotmail.com>
2017-12-26 12:39:11 +0800

    dev

--- src/test/java/hk/quantr/sharepoint/TestDocLib.java
+++ src/test/java/hk/quantr/sharepoint/TestDocLib.java
@@ -60,54 +60,56 @@ public class TestDocLib {
 					}
 				}
 
-				// get all fields
-				HashMap<String, Integer> fieldtypes = new HashMap<String, Integer>();
-				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/fields");
-				if (jsonString != null) {
-					System.out.println(CommonLib.prettyFormatJson(jsonString));
-					json = new JSONObject(jsonString);
-					JSONArray arr = json.getJSONObject("d").getJSONArray("results");
-					for (int x = 0; x < arr.length(); x++) {
-						System.out.println(arr.getJSONObject(x).getString("Title") + "\t\t\t" + arr.getJSONObject(x).getString("InternalName") + "\t\t\t" + arr.getJSONObject(x).getInt("FieldTypeKind"));
-						fieldtypes.put(arr.getJSONObject(x).getString("InternalName"), arr.getJSONObject(x).getInt("FieldTypeKind"));
-					}
-				}
-
-				String query = "";
-				String expand = "&$expand=";
-
-				// get all fields of a specific view
-				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/views/getbytitle('" + SPOnline.escapeSharePointUrl(view) + "')/ViewFields");
-				if (jsonString != null) {
-					System.out.println(CommonLib.prettyFormatJson(jsonString));
-					json = new JSONObject(jsonString);
-					JSONArray arr = json.getJSONObject("d").getJSONObject("Items").getJSONArray("results");
-					for (int x = 0; x < arr.length(); x++) {
-						String fieldName = arr.getString(x);
-						if (fieldtypes.get(fieldName) == 20) {
-							query += fieldName + "/Title,";
-							expand += fieldName + ",";
-						} else {
-							query += fieldName + ",";
-						}
-					}
-				}
-				query += "UniqueId,";
-				query += "GUID,";
-
-				if (query.endsWith(",")) {
-					query = query.substring(0, query.length() - 1);
-				}
-				if (expand.endsWith(",")) {
-					expand = expand.substring(0, expand.length() - 1);
-				}
-
+//				// get all fields
+//				HashMap<String, Integer> fieldtypes = new HashMap<String, Integer>();
+//				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/fields");
+//				if (jsonString != null) {
+//					System.out.println(CommonLib.prettyFormatJson(jsonString));
+//					json = new JSONObject(jsonString);
+//					JSONArray arr = json.getJSONObject("d").getJSONArray("results");
+//					for (int x = 0; x < arr.length(); x++) {
+//						System.out.println(arr.getJSONObject(x).getString("Title") + "\t\t\t" + arr.getJSONObject(x).getString("InternalName") + "\t\t\t" + arr.getJSONObject(x).getInt("FieldTypeKind"));
+//						fieldtypes.put(arr.getJSONObject(x).getString("InternalName"), arr.getJSONObject(x).getInt("FieldTypeKind"));
+//					}
+//				}
+//
+//				String query = "";
+//				String expand = "&$expand=";
+//
+//				// get all fields of a specific view
+//				jsonString = SPOnline.get(token, domain, "test/_api/web/lists/GetByTitle('doclib1')/views/getbytitle('" + SPOnline.escapeSharePointUrl(view) + "')/ViewFields");
+//				if (jsonString != null) {
+//					System.out.println(CommonLib.prettyFormatJson(jsonString));
+//					json = new JSONObject(jsonString);
+//					JSONArray arr = json.getJSONObject("d").getJSONObject("Items").getJSONArray("results");
+//					for (int x = 0; x < arr.length(); x++) {
+//						String fieldName = arr.getString(x);
+//						if (fieldtypes.get(fieldName) == 20) {
+//							query += fieldName + "/Title,";
+//							expand += fieldName + ",";
+//						} else {
+//							query += fieldName + ",";
+//						}
+//					}
+//				}
+//				query += "UniqueId,";
+//				query += "GUID,";
+//
+//				if (query.endsWith(",")) {
+//					query = query.substring(0, query.length() - 1);
+//				}
+//				if (expand.endsWith(",")) {
+//					expand = expand.substring(0, expand.length() - 1);
+//				}
+//
 				// get items of a specific view
 				String filters = "";
 				String url = "test/_api/web/lists/GetByTitle('doclib1')/items?$filter=" + URLEncoder.encode("GUID eq guid'47e1eab5-c06e-46a3-a100-ab0fa3874416'", "utf8");
 //				url = "test/_api/web/lists/GetByTitle('doclib1')/items?$filter=" + URLEncoder.encode("ID eq 13", "utf8");
 				url = "test/_api/web/lists/GetByTitle('doclib1')/items?$select=LinkFilename,GUID,UniqueId&$filter=" + URLEncoder.encode("AuthorId eq 11", "utf8");
 				url = "test/_api/web/lists/GetByTitle('doclib1')/items?$select=LinkFilename,GUID,UniqueId&$filter=" + URLEncoder.encode("UniqueId eq guid'e7ac2b9b-d5e3-4257-a21c-0e2951e83887'", "utf8");
+				//url = "test/_api/web/lists/GetByTitle('doclib1')/items?$expand=Unique&$filter=" + URLEncoder.encode("Unique/Id eq guid'efee39c7-ae90-434a-9174-9a9bd7411c74'", "utf8");
+				url = "test/_api/web/lists/GetByTitle('doclib1')/items?$select=Unique/Id&$expand=Unique";
 //				url = "test/_api/web/lists/GetByTitle('doclib1')/items;
 				System.out.println(url);
 				jsonString = SPOnline.get(token, domain, url);
